7140c739f843dd718aef3e4fc5adf18c374da4e3
Omar Zikry <omar.zikry@zollect.com>
2020-02-10 16:32:00 +0200

    Setup local storage

---
+++ src/utils/localStorage.js
@@ -0,0 +1,21 @@
+// this function will load state from local storage and parse it to json
+export const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem("state");
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return undefined;
+  }
+};
+// this function will save state to the local storage
+export const saveState = state => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("state", serializedState);
+  } catch {
+    // ignore write errors
+  }
+};
