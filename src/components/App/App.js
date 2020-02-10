d33e260e850cba3a9c2d24fb04de18eb23570c9c
Omar Zikry <omar.zikry@zollect.com>
2020-02-10 16:24:47 +0200

    Add Routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,56 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import Login from "../../routes/login";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+import Navbar from '../Navbar/Navbar';
+import People from '../../routes/people';
+import SinglePerson from "../../routes/singlePerson";
+import Favourites from "../../routes/favourites";
 
-function App() {
-  return <div></div>;
-}
+const GlobalStyle = createGlobalStyle`
+${reset}
+  html{
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    font-size: 16px;
+    font-weight: 400;
+  }
+  body{
+    max-width: 450px;
+    margin: 0 auto;
+    padding-top: 5em;
+  }
+`;
+
+const App = props => {
+  return (
+    <Router>
+      <GlobalStyle />
+      <Navbar />
+      <Switch>
+        <Route path="/people/:id">
+          <SinglePerson />
+        </Route>
+        <Route exact path="/people">
+          <People />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/favourites">
+          <Favourites />
+        </Route>
+        <Route exact path="/">
+          <Redirect to="/login" />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
